37da62a42612e5e890c7af82f212b1aaeec2208c
diih <dvinicius0@gmail.com>
2020-08-03 15:22:37 -0300

    First commit

---
+++ src/main/java/com/brascoffee/brascoffee/BrascoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.brascoffee.brascoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrascoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrascoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brascoffee/brascoffee/BrascoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brascoffee.brascoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrascoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
